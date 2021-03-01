592ead382769d990706f7d787e3b6ebe1499b7e9
Yury Zenko <yury.zenko@solbeg.com>
2021-03-01 15:55:01 +0300

    Versions

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "InputValidator",
+    platforms: [
+           .iOS(.v12),
+       ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -24,5 +27,6 @@ let package = Package(
         .testTarget(
             name: "InputValidatorTests",
             dependencies: ["InputValidator"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
