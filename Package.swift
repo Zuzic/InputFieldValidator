5775dd7346a222947a11743e862531838575d6af
Yury Zenko <yury.zenko@solbeg.com>
2021-03-01 16:39:47 +0300

    Added resources

--- Package.swift
+++ Package.swift
@@ -23,7 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "InputValidator",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                        .process("TextRegexes.plist")]),
         .testTarget(
             name: "InputValidatorTests",
             dependencies: ["InputValidator"]),
